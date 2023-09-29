8eb2d71452f6fc31fc33687340aff88043e596d2
fmtk <fiadmtk@protonmail.com>
2023-09-28 21:50:26 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class main {
+import java.util.Scanner;
+import java.text.DecimalFormat;
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("####.##"); // used for rounding the decimals to two decimal places
+        // inputs
+        System.out.println("What is the total bill?");
+        double bill = scan.nextDouble();
+        System.out.println("What is the percent of the tip you want to pay?");
+        int tip = scan.nextInt();
+        System.out.println("How many people are in your group?");
+        int people = scan.nextInt();
+
+        // calculations
+        double tipAmt = bill * ((double) tip*.01);
+        double totalBill = bill + tipAmt;
+        double tipPerPerson = tipAmt / ((double) people);
+        double totalPerPerson = (bill/((double) people) + tipPerPerson);
+
+        //printing out
+        System.out.println("The total tip amount is $" + df.format(tipAmt));
+        System.out.println("The total bill, including the tip is $" +df.format(totalBill));
+        System.out.println("Each person has to pay a tip of $" +df.format(tipPerPerson));
+        System.out.println("The total each person has to pay is $" +df.format(totalPerPerson));
+
+    }
 }
